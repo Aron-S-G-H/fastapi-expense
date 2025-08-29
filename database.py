223edf56c6696c1d8215df85759fc6aeb710685a
Aron Sadegh <95709170+Aron-S-G-H@users.noreply.github.com>
2025-08-29 11:03:44 +0330

    initial

--- database.py
+++ database.py
@@ -1,5 +1,6 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker,declarative_base
+from sqlalchemy import create_engine, String, Float
+from sqlalchemy.orm import sessionmaker,declarative_base, Mapped, mapped_column
+
 
 SQLALCHEMY_DATABASE_URL = "sqlite:///./sqlite.db"
 
@@ -7,5 +8,13 @@ engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
+
+class User(Base):
+    __tablename__ = "expenses"
+    
+    id: Mapped[int] = mapped_column(primary_key=True)
+    description: Mapped[str] = mapped_column(String(255))
+    amount: Mapped[float] = mapped_column(Float)
+
 
 Base.metadata.create_all(engine)
