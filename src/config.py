36596eb8b2b2576a631b81ed4d1cc8addd02780e
Aron Sadegh <95709170+Aron-S-G-H@users.noreply.github.com>
2025-10-03 13:51:45 +0330

    update configs

--- src/config.py
+++ src/config.py
@@ -2,13 +2,13 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Setting(BaseSettings):
-    SQLALCHEMY_DATABASE_URL: str
+    SQLALCHEMY_DATABASE_URL: str = "postgresql+psycopg2://postgres:postgres@db:5432/postgres"
     SECRET_KEY: str
     ALGORITHM: str
     AUTH_MODE: str
     SUPPORTED_LANGS: str
     DEFAULT_LANG: str
-    REDIS_URL: str
+    REDIS_URL: str = "redis://redis:6379"
     SENTRY_DSN: str = "https://11395568eaeae90b40c6cf95202d8801@sentry.hamravesh.com/9091"
 
     model_config = SettingsConfigDict(
